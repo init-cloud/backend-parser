c49ece40090e2078d259fcc5ac15ef5495f2e4df
floodnut <gsniper777@gmail.com>
2022-11-01 22:58:01 +0900

    [22.11.01][add] add parent parser

--- service/base_parser.py
+++ service/base_parser.py
@@ -1,15 +1,56 @@
+from service.enum.blocks import Block
+
 class BaseParser():
 
     def __init__(
-        self, 
-        file
+        self,
+        data: list = [],
+        local: list = [],
+        module: list = [],
+        output: list = [],
+        provider: list = [],
+        resource: list = [],
+        terraform: list = [],
+        variable: list = []
     ) -> None:
-        self.file = file
-        datas     = []
-        local     = []
-        module    = []
-        output    = []
-        provider  = []
-        resource  = []
-        terraform = []
-        variable  = []
+        self.data      = data 
+        self.local     = local
+        self.module    = module
+        self.output    = output
+        self.provider  = provider
+        self.resource  = resource
+        self.terraform = terraform
+        self.variable  = variable
+
+    def add(
+        self,
+        data: list=[],
+        local: list=[],
+        module: list=[],
+        output: list=[],
+        provider: list=[],
+        resource: list=[],
+        terraform: list=[],
+        variable: list=[]
+    ) -> None:
+        self.data += data
+        self.local += local
+        self.module += module
+        self.output += output
+        self.provider += provider
+        self.resource += resource
+        self.terraform += terraform
+        self.variable += variable
+
+    def find(
+        self,
+        va: list,
+        type: str=None
+    ) -> str:
+        if type == Block.DATA:
+            return va[1] + "." + va[2]
+        elif type == Block.VARIABLE:
+            return va[1]
+        else:
+            result = (va[0] + "." + va[1]).split("[")
+            return result[0]
--- service/impl/parser.py
+++ service/impl/parser.py
@@ -1,11 +1,13 @@
+from service.base_parser import BaseParser
 from service.common.response_entity import Response
 from service.enum.blocks import Block, NCPResource
 import os, hcl2
 
 
-class TFParser():
+class TFParser(BaseParser):
     def __init__(self, block=[]) -> None:
         self.block = block
+        super().__init__()
 
     def load_file(self, path: str) -> list:
         for (root, directories, files) in os.walk(f'uploads/{path}/'):
@@ -16,13 +18,25 @@ class TFParser():
 
                 with open(file_path, 'r') as tf:
                     fs = hcl2.load(tf)
+                    self.add(
+                        data=fs[Block.DATA],
+                        resource=fs[Block.RESOURCE],
+                        variable=fs[Block.VARIABLE])
+
                     self.block.append(fs)
-        
+
         return self.block
 
-    ##
-    # temp
-    ##
+    def get_value(self, var: str) -> str:
+        v = var.split(".")
+
+        if not v:
+            return None
+        
+        result = self.find(v, v[0])
+
+        return result
+
     def get_var(self, var) -> str:
         if isinstance(var, list) and var:
             if var[0].startswith("$"):
@@ -34,37 +48,40 @@ class TFParser():
                 return var[2:-1]
             else:
                 return var
+
         return None
 
     def get_block_value(self, files: list) -> list:
-        res = []
+        result = []
+
         for file in files:
             for k in file.keys():
                 if k == Block.RESOURCE:
                     for d in file[k]:
                         if NCPResource.NCLOUD_SUBNET in d:
-                            res.append({ "data" : self.parse(d[NCPResource.NCLOUD_SUBNET],
+                            result.append({ "data" : self.parse(d[NCPResource.NCLOUD_SUBNET],
                                                 b_type=NCPResource.NCLOUD_SUBNET)[0]})
                         elif NCPResource.NCLOUD_SERVER in d:
-                            res.append({ "data" : self.parse(d[NCPResource.NCLOUD_SERVER],
+                            result.append({ "data" : self.parse(d[NCPResource.NCLOUD_SERVER],
                                                 b_type=NCPResource.NCLOUD_SERVER)[0]})
                         elif NCPResource.NCLOUD_LB_TARGET_GROUP in d:
-                            res.append({ "data" : self.parse(d[NCPResource.NCLOUD_LB_TARGET_GROUP],
+                            result.append({ "data" : self.parse(d[NCPResource.NCLOUD_LB_TARGET_GROUP],
                                                 b_type=NCPResource.NCLOUD_LB_TARGET_GROUP)[0]})
                         elif NCPResource.NCLOUD_ACCESS_CONTROL_GROUP in d:
-                            res.append({ "data" : self.parse(d[NCPResource.NCLOUD_ACCESS_CONTROL_GROUP],
+                            result.append({ "data" : self.parse(d[NCPResource.NCLOUD_ACCESS_CONTROL_GROUP],
                                                 b_type=NCPResource.NCLOUD_ACCESS_CONTROL_GROUP)[0]})
                         elif NCPResource.NCLOUD_NETWORK_ACL in d:
-                            res.append({ "data" : self.parse(d[NCPResource.NCLOUD_NETWORK_ACL],
+                            result.append({ "data" : self.parse(d[NCPResource.NCLOUD_NETWORK_ACL],
                                                 b_type=NCPResource.NCLOUD_NETWORK_ACL)[0]})
                         elif NCPResource.NCLOUD_NETWORK_INTERFACE in d:
-                            res.append({ "data" : self.parse(d[NCPResource.NCLOUD_NETWORK_INTERFACE],
+                            result.append({ "data" : self.parse(d[NCPResource.NCLOUD_NETWORK_INTERFACE],
                                                 b_type=NCPResource.NCLOUD_NETWORK_INTERFACE)[0]})
                         else:
                             continue
-        return res
 
-    def parse(self, blocks, b_type=None) -> list:
+        return result
+
+    def parse(self, blocks: dict, b_type=None) -> list:
         result = []
 
         for k in blocks.keys():
@@ -72,15 +89,12 @@ class TFParser():
 
             if "subnet_no" in block:
                 result.append(
-                    Response(k, b_type, parent=self.get_var(block["subnet_no"]))
-                )
+                    Response(k, b_type, parent=self.get_value(self.get_var(block["subnet_no"]))))
             elif "vpc_no" in block:
                 result.append(
-                    Response(k, b_type, parent=self.get_var(block["vpc_no"]))
-                )
+                    Response(k, b_type, parent=self.get_value(self.get_var(block["vpc_no"]))))
             elif "region" in block:
                 result.append(
-                    Response(k, b_type, parent=self.get_var(block["region"]))
-                )
-                
+                    Response(k, b_type, parent=self.get_value(self.get_var(block["region"]))))
+
         return result
